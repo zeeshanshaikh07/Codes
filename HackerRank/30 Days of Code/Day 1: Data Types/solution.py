1ace493974787352d08dc82ccc06f4f96cd57560
Zeeshan Shaikh <63455236+zeeshanshaikh07@users.noreply.github.com>
2021-05-09 10:34:39 +0530

    solve python - Day 1

---
+++ HackerRank/30 Days of Code/Day 1: Data Types/solution.py
@@ -0,0 +1,16 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+var_int = int(input())
+var_float = float(input())
+var_str = input()
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+print(i + var_int)
+# Print the sum of the double variables on a new line.
+print(d + var_float)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s + var_str)
