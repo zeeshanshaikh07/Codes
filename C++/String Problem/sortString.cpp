d54f3f02f30c7a05bbfd9e10ec3a436941280c27
Zeeshan Shaikh <zeeshanshaikh790.zs@gmail.com>
2021-06-06 12:04:21 +0530

    Add files via upload

---
+++ C++/String Problem/sortString.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void sortString(string &str)
+{
+    sort(str.begin(), str.end());
+    cout << str;
+}
+int main()
+{
+    string s;
+    cin>>s;
+    sortString(s);
+    return 0;
+}
