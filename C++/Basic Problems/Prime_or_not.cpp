c20e2649dbacdd51a6e3a4f87f4c53a794f6f2b8
Zeeshan Shaikh <zeeshanshaikh790.zs@gmail.com>
2021-06-06 12:05:05 +0530

    Create Prime_or_not.cpp

---
+++ C++/Basic Problems/Prime_or_not.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+// #include<cmath> -->  this for commented Line no. 11
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    bool flag = 0;
+
+    for(int i=2;i<n;i++){     // for making short we can also use for(int i=2;i<=sqrt(n);i++)
+        if(n%i == 0){
+            cout<<"Non Prime"<<endl;
+            flag = 1;
+            break;
+        }
+    }
+    if(flag == 0){
+        cout<<"Prime"<<endl;
+    }
+    return 0;
+}
